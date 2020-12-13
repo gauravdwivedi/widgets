90b9ffc51bee5fcdda8ecdc684de312c75ed3651
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-13 22:27:46 +0530

    added Header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Header =()=>{
+    return (
+        <div className="ui secondary pointing menu">
+            <a href="/" className="item">
+                Accordion
+            </a>
+            <a href="/list" className="item">
+                List
+            </a>
+            <a href="/dropdown" className="item">
+                DropDown
+            </a>
+            <a href="/translate" className="item">
+                Translate
+            </a>
+        </div>
+    )
+}
+
+export default Header
