1772a8e8b22a72b14e16fdefbb674d8eb534a13e
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-13 21:37:36 +0530

    translated widget

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
+import Translate from './components/Translate'
 
 const items =[
     {
@@ -37,18 +38,12 @@ const options =[
 const App=()=>{
 
     const [selected,setSelected]=useState(options[0])
-    const [showDropdown,setShowDropdown]=useState(true)
+    // const [showDropdown,setShowDropdown]=useState(true)
 
-    return(<div>
-        <button onClick={()=>setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-        {showDropdown ?
-            <Dropdown 
-            options={options} 
-            onSelectedChange ={setSelected}
-            selected={selected} 
-                />:null
-    }
-    </div>)
+    return(
+            <Translate/>
+       
+    )      
 }
 
 export default App
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect,useRef } from 'react'
 
 
-const Dropdown = ({options,selected,onSelectedChange})=>{
+const Dropdown = ({options,selected,onSelectedChange,label})=>{
     const [open,setOpen]= useState(false)
     const ref = useRef();
 
@@ -39,7 +39,7 @@ const Dropdown = ({options,selected,onSelectedChange})=>{
         return (
             <div ref={ref} className=" ui form">
                 <div className="field">
-                    <label className="label">Select a Color</label>
+                    <label className="label">{label}</label>
                     <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open?'visible active':''}`}>
                         <i className="dropdown icon"></i>
                         <div className="text" style={{color:`${selected.value}`}}>{selected.label}</div>
